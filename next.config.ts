5b21c636609de2ce0f5333a199045acb40186831
Aleksandar Timic <aleksandartimic@gmail.com>
2026-02-01 20:59:08 +0100

    Config update

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,13 @@ const nextConfig: NextConfig = {
       },
     },
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      use: ["raw-loader"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
